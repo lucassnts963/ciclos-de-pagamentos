bb82f01d76cfe13e96d36e9193adf5b848c4f8a4
Lucas Santos <lucassnts963@gmail.com>
2020-04-18 15:04:17 -0300

    Atualizações do dia  18/04/2020

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './main/App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { applyMiddleware, createStore } from 'redux'
+import {Provider} from 'react-redux'
+
+import promise from 'redux-promise'
+
+
+import App from './main/App'
+import reducers from './main/reducers'
+import * as serviceWorker from './serviceWorker'
+
+
+const store = applyMiddleware(promise)(createStore)(reducers)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -5,12 +5,14 @@ import Header from '../common/template/header'
 import SideBar from '../common/template/sideBar'
 import Footer from '../common/template/footer'
 
+import Routes from './routes'
+
 export default props => (
     <div className='wrapper'>
         <Header/>
         <SideBar/>
         <div className='content-wrapper'>
-            <h1>Conteudo</h1>
+            <Routes/>
         </div>
         <Footer />
     </div>
